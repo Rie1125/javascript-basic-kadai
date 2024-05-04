d585e3cf9b4dd98561940bf0ffba72afd89c9f49
Rie1125 <162334496+Rie1125@users.noreply.github.com>
2024-05-05 00:18:57 +0900

    kadai_021をプッシュ

---
+++ kadai_021/event.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+  let text = document.getElementById('text');
+  text.textContent = 'ボタンをクリックしました';
+  },2000);
+});
